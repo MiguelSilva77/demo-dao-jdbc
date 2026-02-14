1d950d527e1954cf51172c074ba7e0585519e1bc
Miguel Silva <miguelmartins.migas@gmail.com>
2026-02-14 20:49:26 -0300

    create class seller

---
+++ src/entities/Seller.java
@@ -0,0 +1,112 @@
+package entities;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.Objects;
+
+import model.entities.Department;
+
+public class Seller implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	private int id;
+	private String name;
+	private String email;
+	private LocalDate BrithDate;
+	private double baseSalary;
+	
+	private Department department;
+	
+	public Seller(){
+	}
+
+	public Seller(int id, String name, String email, LocalDate brithDate, double baseSalary, Department department) {
+		this.id = id;
+		this.name = name;
+		this.email = email;
+		BrithDate = brithDate;
+		this.baseSalary = baseSalary;
+		this.department = department;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public LocalDate getBrithDate() {
+		return BrithDate;
+	}
+
+	public void setBrithDate(LocalDate brithDate) {
+		BrithDate = brithDate;
+	}
+
+	public double getBaseSalary() {
+		return baseSalary;
+	}
+
+	public void setBaseSalary(double baseSalary) {
+		this.baseSalary = baseSalary;
+	}
+
+	public Department getDepartment() {
+		return department;
+	}
+
+	public void setDepartment(Department department) {
+		this.department = department;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Seller other = (Seller) obj;
+		return id == other.id;
+	}
+
+	@Override
+	public String toString() {
+		return "Seller [id=" + id + ", name=" + name + ", email=" + email + ", BrithDate=" + BrithDate + ", baseSalary="
+				+ baseSalary + ", department=" + department + "]";
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
--- src/program/Main.java
+++ src/program/Main.java
@@ -1,5 +1,8 @@
 package program;
 
+import java.time.LocalDate;
+
+import entities.Seller;
 import model.entities.Department;
 
 public class Main {
@@ -7,6 +10,9 @@ public class Main {
 	public static void main(String[] args) {
 		Department ob = new Department(1, "books");
 		System.out.println(ob);
+		
+		Seller seller = new Seller(21, "bob", "bob@gmail", LocalDate.now(), 2000, ob);
+		System.out.println(seller);
 	}
 
 }
