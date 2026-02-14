90b1273a249403b9391633bdfd93f09aa0a96068
Miguel Silva <miguelmartins.migas@gmail.com>
2026-02-14 20:09:43 -0300

    primeiro commit

---
+++ src/db/DbIntegerException.java
@@ -0,0 +1,11 @@
+package db;
+
+public class DbIntegerException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+	
+	public DbIntegerException(String msg) {
+		super(msg);
+	}
+
+}
