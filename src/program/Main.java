2244d3f88e2eed503f2bf22f451d67066574ba8d
Miguel Silva <miguelmartins.migas@gmail.com>
2026-02-14 20:33:04 -0300

    creat epartment class

---
+++ src/program/Main.java
@@ -0,0 +1,12 @@
+package program;
+
+import model.entities.Department;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Department ob = new Department(1, "books");
+		System.out.println(ob);
+	}
+
+}
